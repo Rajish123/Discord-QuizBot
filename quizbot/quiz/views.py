9443a74b3c06905bca5b3eb37442be05433bda7f
rajish <maharjanrajish101@gmail.com>
2022-10-07 16:57:14 +0545

    RandomQuestionAPIView

--- quizbot/quiz/views.py
+++ quizbot/quiz/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Question
+from .serializers import RandomQuestionSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+class RandomQuestion(APIView):
+
+    def get(self,request,format = None, **kwargs):
+        # selecting all the questions and ordering randomly and selection top answer one at a time
+        question = Question.objects.filter().order_by('?')[:1]
+        serializer = RandomQuestionSerializer(question, many = True)
+        return Response(serializer.data)
+
